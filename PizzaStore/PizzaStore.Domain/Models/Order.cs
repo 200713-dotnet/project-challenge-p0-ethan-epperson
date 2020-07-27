2ed5c35a97f86b2306dbcd080167cca73964ebbd
ethan epperson <ethanepperson5@gmail.com>
2020-07-26 21:29:17 -0700

    added a price limit to orders

--- PizzaStore/PizzaStore.Domain/Models/Order.cs
+++ PizzaStore/PizzaStore.Domain/Models/Order.cs
@@ -10,25 +10,46 @@ namespace PizzaStore.Domain.Models
         public DateTime DateOrdered {get; set;}
 
         private int NumberOfPizzasOrdered;
+
+        private double TotalCostOfOrder;
         
         public void CreatePizza(string size, string crust, List<string> toppings)
         {
-           if(NumberOfPizzasOrdered < 50)
-           {
-                Pizzas.Add(new Pizza(size, crust, toppings));
-                NumberOfPizzasOrdered = NumberOfPizzasOrdered + 1;
-           }
-           else
-           {
-               System.Console.WriteLine("Sorry we can't make more then 50 pizzas at a time.");
-           }
+            if(NumberOfPizzasOrdered < 50)
+            {
+                Pizza p = new Pizza(size, crust, toppings);
+                    if(TotalCostOfOrder + p.ComputePricing() < 250)
+                    {
+                        Pizzas.Add(p);
+                        NumberOfPizzasOrdered = NumberOfPizzasOrdered + 1;
+                        TotalCostOfOrder = TotalCostOfOrder + p.ComputePricing();
+                    }
+                    else
+                    {
+                        System.Console.WriteLine();
+                        System.Console.WriteLine("Sorry but orders can't go over $250");
+                        System.Console.WriteLine("Trust us, its for your own good.");
+
+                    }
+                        
+            }
+            else
+            {
+                System.Console.WriteLine();
+                System.Console.WriteLine("Sorry we can't make more then 50 pizzas at a time.");
+            }
+            
         }
 
         public void DestroyPizza(int i)
         {
             i = i - 1;
+            Pizza p = Pizzas[i];
+            double removeCost = p.ComputePricing();
             Pizzas.RemoveAt(i);
             NumberOfPizzasOrdered = NumberOfPizzasOrdered - 1;
+            TotalCostOfOrder = TotalCostOfOrder - removeCost;
+            
         }
 /*
         public void DisplayOrder()
@@ -47,6 +68,7 @@ namespace PizzaStore.Domain.Models
         {
             Pizzas = new List<Pizza>();
             NumberOfPizzasOrdered = 0;
+            TotalCostOfOrder = 0;
         }
     }
 }
